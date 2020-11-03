5037e2878b2f5d6e229bbaa9213ec8ff5ac593fe
ERika-tech257 <ErikaJtech@gmail.com>
2020-11-02 19:06:55 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Location from './components/Location/Location'
+import Weather from './components/Weather/Weather'
 
 function App() {
   return (
     <div className="App">
       <Location />
+      <br/>
+      <Weather />
     </div>
   );
 }
